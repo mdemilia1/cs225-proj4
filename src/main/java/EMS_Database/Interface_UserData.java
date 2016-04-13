4493c61ae866c451a897aebed3b6a1b509419613
Bill <wrconner92@gmail.com>
2016-04-13 15:52:49 -0400

    Added permissions to UserData_Table

--- src/main/java/EMS_Database/Interface_UserData.java
+++ src/main/java/EMS_Database/Interface_UserData.java
@@ -1,6 +1,8 @@
 package EMS_Database;
 
 import BackEnd.UserSystem.Address;
+import auth.AuthorizationException;
+
 import java.util.ArrayList;
 
 /**
@@ -10,74 +12,74 @@ import java.util.ArrayList;
 public interface Interface_UserData {
 
     // SPECIAL FUNCTIONS
-    public int createUser(InputUser user);        
+    public int createUser(InputUser user) throws AuthorizationException;
     
-    public int nextValidUID();
+    public int nextValidUID() throws AuthorizationException;
     
-    public void removeUser(int uid) throws DoesNotExistException;        
+    public void removeUser(int uid) throws DoesNotExistException, AuthorizationException;
         
 
     // GETTERS
     //public User getUser(int uid) throws DoesNotExistException;
     
-    public ArrayList<Integer> getParticipantList() throws DoesNotExistException;
+    public ArrayList<Integer> getParticipantList() throws DoesNotExistException, AuthorizationException;
     
-    public int getUIDByEmail(String email) throws DoesNotExistException;
+    public int getUIDByEmail(String email) throws DoesNotExistException, AuthorizationException;
 
-    public String getFirstName(int uid) throws DoesNotExistException;
+    public String getFirstName(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getLastName(int uid) throws DoesNotExistException;
+    public String getLastName(int uid) throws DoesNotExistException, AuthorizationException;
 
-    public String getEmail(int uid) throws DoesNotExistException;
+    public String getEmail(int uid) throws DoesNotExistException, AuthorizationException;
 
-    public String getPwd(int uid) throws DoesNotExistException;
+    public String getPwd(int uid) throws DoesNotExistException, AuthorizationException;
 
-    public int getLevel(int uid) throws DoesNotExistException;
+    public int getLevel(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getPhone(int uid) throws DoesNotExistException;
+    public String getPhone(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getStreet(int uid) throws DoesNotExistException;
+    public String getStreet(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getCity(int uid) throws DoesNotExistException;
+    public String getCity(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getState(int uid) throws DoesNotExistException;
+    public String getState(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getZipcode(int uid) throws DoesNotExistException;
+    public String getZipcode(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public String getCountry(int uid) throws DoesNotExistException;
+    public String getCountry(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public int getEventCreationPrivilege(int uid) throws DoesNotExistException;
+    public int getEventCreationPrivilege(int uid) throws DoesNotExistException, AuthorizationException;
     
-    public boolean getParticipant(int uid) throws DoesNotExistException;
+    public boolean getParticipant(int uid) throws DoesNotExistException, AuthorizationException;
     
     
     //SETTERS   
-    public void setAddress(int uid , Address address) throws DoesNotExistException;   
+    public void setAddress(int uid , Address address) throws DoesNotExistException, AuthorizationException;
 
-    public void setFirstName(int uid, String fname) throws DoesNotExistException;
+    public void setFirstName(int uid, String fname) throws DoesNotExistException, AuthorizationException;
     
-    public void setLastName(int uid, String lname) throws DoesNotExistException;
+    public void setLastName(int uid, String lname) throws DoesNotExistException, AuthorizationException;
 
-    public void setEmail(int uid, String email) throws DoesNotExistException;
+    public void setEmail(int uid, String email) throws DoesNotExistException, AuthorizationException;
 
-    public void setPwd(int uid, String pwd) throws DoesNotExistException;
+    public void setPwd(int uid, String pwd) throws DoesNotExistException, AuthorizationException;
 
-    public void setLevel(int uid, int level) throws DoesNotExistException;
+    public void setLevel(int uid, int level) throws DoesNotExistException, AuthorizationException;
     
-    public void setPhone(int uid, String phone) throws DoesNotExistException;
+    public void setPhone(int uid, String phone) throws DoesNotExistException, AuthorizationException;
     
-    public void setStreet(int uid, String street) throws DoesNotExistException;
+    public void setStreet(int uid, String street) throws DoesNotExistException, AuthorizationException;
     
-    public void setCity(int uid, String city) throws DoesNotExistException;
+    public void setCity(int uid, String city) throws DoesNotExistException, AuthorizationException;
     
-    public void setState(int uid, String state) throws DoesNotExistException;
+    public void setState(int uid, String state) throws DoesNotExistException, AuthorizationException;
     
-    public void setZipcode(int uid, String zipcode) throws DoesNotExistException;
+    public void setZipcode(int uid, String zipcode) throws DoesNotExistException, AuthorizationException;
     
-    public void setCountry(int uid, String country) throws DoesNotExistException;
+    public void setCountry(int uid, String country) throws DoesNotExistException, AuthorizationException;
     
-    public void setEventCreationPrivilege(int uid, int level) throws DoesNotExistException;
+    public void setEventCreationPrivilege(int uid, int level) throws DoesNotExistException, AuthorizationException;
     
-    public void setParticipant(int uid , boolean status) throws DoesNotExistException;
+    public void setParticipant(int uid , boolean status) throws DoesNotExistException, AuthorizationException;
     
 }
