df7e069738d940f781968f981819e78b81f9485b
Paul Buonopane <paul+git@earth2me.com>
2016-04-23 00:25:34 -0400

    Remove example code

--- src/main/java/auth/levels/LoggedOutLevel.java
+++ src/main/java/auth/levels/LoggedOutLevel.java
@@ -14,52 +14,36 @@ public class LoggedOutLevel extends Level {
 
     @Override
     protected void checkPermissionUsers(Context context) throws AuthorizationException {
-        // There are a lot of special methods fo checking user table permissions.
-        switch (context.getFieldForSwitch()) {
-            case "":
-                // Normally, only CREATE and DELETE have empty fields, but VIEW and MODIFY should be accounted
-                // for, just in case.  When an entry is created/deleted, all fields are affected, so there's no point
-                // in running a check for each one individually.
-                context
-                        .cannotViewOther()    // "Other" means other users; they can still view info about themselves
-                        .cannotModify()       // No "Other" or "Self"; doesn't matter who the user is that's being modified
-                        .cannotCreate()       // User can't add other users.
-                        .cannotDeleteOther(); // User can delete self.  Opposite is cannotDeleteSelf().
-                break;
-
-            case "EXAMPLE1":
-                context.noAccess();  // Shortcut; denies everything
-                break;
-
-            case "EXAMPLE2":
-                // There are also fancy methods like these that prevent users from modifying other users with
-                // the same or greater privilege levels:
-                context
-                        .cannotCreateWithSameOrGreaterLevel()
-                        .cannotModifyOtherWithGreaterLevel();
-                break;
-
-            // TODO
-
-            default:
-                throw new UnknownFieldException(context);
-        }
+        context.noAccess();
     }
 
     @Override
     protected void checkPermissionEvents(Context context) throws AuthorizationException {
-        /*
-         * Here, we would only use the simple methods, because this isn't the user table.  The basics are:
-         *   - cannotView
-         *   - cannotModify
-         *   - cannotCreate
-         *   - cannotDelete
-         */
-
-        // TODO
+        context.noAccess();
     }
 
-    ////// And there are more methods. ///////
-    // You'll need to use your IDE to automatically generate stubs for them.
-    // WHEN YOU ARE DONE, THERE SHOULDN'T BE ANY ERRORS IN YOUR FILE.
+    @Override
+    protected void checkPermissionSubEvents(Context context) throws AuthorizationException {
+        context.noAccess();
+    }
+
+    @Override
+    protected void checkPermissionCommittee(Context context) throws AuthorizationException {
+        context.noAccess();
+    }
+
+    @Override
+    protected void checkPermissionTasks(Context context) throws AuthorizationException {
+        context.noAccess();
+    }
+
+    @Override
+    protected void checkPermissionIncome(Context context) throws AuthorizationException {
+        context.noAccess();
+    }
+
+    @Override
+    protected void checkPermissionExpense(Context context) throws AuthorizationException {
+        context.noAccess();
+    }
 }
--- src/main/java/auth/levels/SystemLevel.java
+++ src/main/java/auth/levels/SystemLevel.java
@@ -15,4 +15,39 @@ public class SystemLevel extends Level {
     public void checkPermission(String table, String field, Operation operation, Who who, PrivilegeLevel otherLevel) throws AuthorizationException {
         // Allow everything
     }
+
+    @Override
+    protected void checkPermissionUsers(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionEvents(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionSubEvents(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionCommittee(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionTasks(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionIncome(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
+
+    @Override
+    protected void checkPermissionExpense(Context context) throws AuthorizationException {
+        // Intentionally blank.
+    }
 }
